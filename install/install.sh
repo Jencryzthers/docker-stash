ebf5bea51b535f0916a6a4730cb5e3bfe9bb3c79
Jean-Christophe Proulx <jcproulx@gox.ca>
2014-12-02 15:33:04 -0500

    Updated to Stash 3.5.0 Removed MySQL/MariabDB auto-install

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-echo "Starting installation of Stash..."
+echo "Starting installation of atlassian-stash-3.5.0-x64..."
 cd /data 
-wget -q http://www.atlassian.com/software/stash/downloads/binary/atlassian-stash-3.2.0-x64.bin
-chmod +x atlassian-stash-3.2.0-x64.bin
-./atlassian-stash-3.2.0-x64.bin -q
+wget -q http://www.atlassian.com/software/stash/downloads/binary/atlassian-stash-3.5.0-x64.bin
+chmod +x atlassian-stash-3.5.0-x64.bin
+./atlassian-stash-3.5.0-x64.bin -q
 exit 0
